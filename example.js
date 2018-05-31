fd71b1574748a449cff448142838c5f76781f5d3
Matthew C. Tedder <matthewct@gmail.com>
2018-05-30 22:29:30 -0400

    matchers working with variables and inbetweens

--- example.js
+++ example.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node 
 
+var fs = require('fs');
 var {Moringa} = require('./moringa.js');
-var agent = new Moringa( reception );
+var agent = new Moringa( reception, 'synthia' );
 
 // Import Moringa Script
-let script = 'recognizer "hello"\noption\nsay "How are you?"\n';
-agent.importFoundation( script, 'Cybernetic Cynthia' );
+console.log('\nStarting Cybernetic Synthia..');
+console.log('=============================');
+let script = fs.readFileSync('./synthia.pgm').toString();
+
+agent.importFoundation( script, 'synthia' );
 
 // Send message to agent and route response to reception
-agent.input('Hello');
+//agent.input('apple suger cookie is orange marmalade','synthia');
+agent.input('I only want to say hello, my friend','synthia');
 
 // Function to Receive Agent Output (callback and/or manual)
 function reception(message) {
--- moringa.js
+++ moringa.js
@@ -5,7 +5,7 @@
 var tokenizer = require('retokenizer');
 
 class Moringa {
-	//constructor( callback, name = 'myself', script = 'Recognizer "[anything]"\nSay "I am brainless"\n' ) {
+	//constructor( callback, name = 'myself', script = '' ) {
 	constructor( callback, name = 'myself', script = '' ) {
 		this.callback    = callback;  // where to send interjections
 		this.name        = name;      // name of base personality 
@@ -14,7 +14,7 @@ class Moringa {
 		if( script !== '' ) this.importFoundation( script, name );
 	}
 
-	importFoundation( script, name = 'Foundation' ) {
+	importFoundation( script, name = this.name ) {
 		this.personality[name] = [];
 		this.personality[name].push({
 				name:'base',     // base context of personality (interpreted after any other active context(s))
@@ -22,10 +22,10 @@ class Moringa {
 				memories:[],     // context's memories
 				active:true
 		});
-		this.importSupplement( name, script );
+		this.importSupplement( script, name );
 	}
 
-	importSupplement( name, script ) {
+	importSupplement( script, name ) {
 		if( this.personality[name] === undefined ) return 'Failure: Specified foundation "' + name + '" could not be supplemented because it does not exist.'; 
 		var personality = this.personality[name];   // each personality comprises an array of contexts 
 		var context     = personality[0];           // base context of personality 
@@ -34,7 +34,7 @@ class Moringa {
 		var syntax = {
 			splitters:[
 				' ','\t','\n','--',
-				'recognizer','last','exclusive','option',
+				'recognizer','fallback','nonexclusive','option',
 				'<>','>=','<=','>','<','=','(',')','and','or','not','+','-',
 				'synonym','group',':',',','conjugate','maintain',
 				'say','remember','recall','forget','interpret','expect','as','request',
@@ -42,56 +42,39 @@ class Moringa {
 				'in','at','years','months','weeks','days','hours','minutes','seconds'
 			],
 			removes:[' ','\t'],
-			enclosures:[
-				{ opener:'"', escaper:'\\', closer:'"' },
-				{ opener:'[', escaper:'\\', closer:']' }
-			]
+			enclosures:[{ opener:'"', escaper:'\\', closer:'"' }]
 		};
 
 		let tokens = tokenizer( script, syntax, true );
 		var i = 0;
 		//console.log( JSON.stringify( tokens, null, '  ' ) );
 		
-		/* Compile statements 
-		// recognizers[
-		 	{
-		 	pattern:'',
-		 	priority:0,
-		 	options:[
-		 		{
-		 			last:false,
-		 			exclusive:false,
-		 			condition:'',
-		 			actions:[]
-		 		},..
-		 		]
-		 	},..
-		 	]
-		*/
 		// Begin with assumption we are in global recognizer's global option
-		var recognizer = { pattern:'', priority:0, options:[] };                     // always executed and always first (per interaction)
-		var option     = { last:false, exclusive:false, condition:'', actions:[] };  // always executed and always first (per option)
+		var recognizer = { pattern:'', matchers:[], options:[], actions:[] };
+		var option     = recognizer;  // to collect recognizer's always actions
+		context.recognizers.push(recognizer);
 
 		var commands = [
-			{ command:'comment',     gram:'-- * \n',                          param:{ message:1 }                        },  // useful to export
-			{ command:'context',     gram:'context " * " \n',                 param:{ context:2 }                        },
-			{ command:'recognizer',  gram:'recognizer " * " \n',              param:{ pattern:2 }                        },
-			{ command:'optionif',    gram:'?last ?exclusive option if * \n',  param:{}                                   },  // use logic to find params 
-			{ command:'option',      gram:'?last ?exclusive option \n',       param:{}                                   },  // use logic to find params
-			{ command:'synonym',     gram:'synonym * : * ?timing\n',          param:{ keyword:1, alternates:2, timing:3} },
-			{ command:'group',       gram:'group * : * ?timing \n',           param:{ keyword:1, alternates:2, timing:3} },
-			{ command:'conjugate',   gram:'conjugate * \n',                   param:{ transforms:1 }                     }, 
-			{ command:'maintain',    gram:'maintain * ?timing \n',            param:{ variable:1, timing:2 }             },
-			{ command:'say',         gram:'say " * " ?timing \n',             param:{ message:2, timing:4 }              },
-			{ command:'remember',    gram:'remember " * " ?timing \n',        param:{ message:2, timing:4 }              },
-			{ command:'recall',      gram:'recall " * " ?timing\n',           param:{ message:2, timing:4 }              },
-			{ command:'forget',      gram:'forget " * " ?timing \n',          param:{ message:2, timing:4 }              },
-			{ command:'interpretas', gram:'interpret as " * " ?timing \n',    param:{ statement:3, timing:5 }            },
-			{ command:'expectas',    gram:'expect " * " as " * " ?timing \n', param:{ expecting:2, as:6, timing:8 }      },
-			{ command:'request',     gram:'request " * " ?timing \n',         param:{ url:2, timing:4 }                  },
-			{ command:'enter',       gram:'enter " * " \n',                   param:{ context:2 }                        },
-			{ command:'exit',        gram:'exit " * " \n',                    param:{ context:2 }                        },
-			{ command:'blankspace',  gram:'\n',                               param:{}                                   }
+			{ command:'comment',     gram:'-- * \n',                                param:{ message:1 }                        },  // useful to export
+			{ command:'context',     gram:'context " * " ?:',                       param:{ context:2 }                        },
+			{ command:'recognizer',  gram:'recognizer " * " ?:',                    param:{ pattern:2 }                        },
+			{ command:'alwaysif',    gram:'?nonexclusive always if ?: * \n',        param:{}                                   },  // use logic to find params 
+			{ command:'optionif',    gram:'?fallback ?nonexclusive option if ?: * \n',  param:{}                                   },  // use logic to find params 
+			{ command:'option',      gram:'?fallback ?nonexclusive option ?:',          param:{}                                   }, 
+			{ command:'synonym',     gram:'synonym * : * ?timing\n',                param:{ keyword:1, alternates:2, timing:3} },
+			{ command:'group',       gram:'group * : * ?timing \n',                 param:{ keyword:1, alternates:2, timing:3} },
+			{ command:'conjugate',   gram:'conjugate * \n',                         param:{ transforms:1 }                     }, 
+			{ command:'invert',      gram:'invert * \n',                            param:{ transforms:1 }                     }, 
+			{ command:'retain',      gram:'retain * ?timing \n',                    param:{ variable:1, timing:2 }             },
+			{ command:'say',         gram:'say " * " ?timing \n',                   param:{ message:2, timing:4 }              },
+			{ command:'remember',    gram:'remember " * " ?timing \n',              param:{ message:2, timing:4 }              },
+			{ command:'recall',      gram:'recall " * " ?timing\n',                 param:{ message:2, timing:4 }              },
+			{ command:'forget',      gram:'forget " * " ?timing \n',                param:{ message:2, timing:4 }              },
+			{ command:'interpretas', gram:'interpret as " * " ?timing \n',          param:{ statement:3, timing:5 }            },
+			{ command:'expectas',    gram:'expect " * " as " * " ?timing \n',       param:{ expecting:2, as:6, timing:8 }      },
+			{ command:'enter',       gram:'enter " * " ?timing \n',                 param:{ context:2 }                        },
+			{ command:'exit',        gram:'exit " * " ?timing \n',                  param:{ context:2 }                        },
+			{ command:'blankspace',  gram:'\n',                                     param:{}                                   }
 		];
 
 		// Loop through Code Tokens
@@ -162,7 +145,7 @@ class Moringa {
 
 			// Grammar Unrecognized
 			if( found === undefined ) {
-				err = 'Malformed Statement at ' + JSON.stringify(tokens[t].value) + ', line ' + tokens[t].lineNo + '.';
+				err = 'Malformed Statement at ' + JSON.stringify(tokens[t].value) + ', line ' + ( tokens[t].lineNo + 1 ) + '.';
 				break;
 			}
 			
@@ -178,25 +161,25 @@ class Moringa {
 				// Architectural Commands
 				case 'context':
 					context = {name:found.tokens[2].value, recognizers:[], memories:[], active:false}; 
+					recognizer = { pattern:'', matchers:[], options:[], actions:[] };  // context's global always recognizer 
+					option     = recognizer;                                           // to collect recognizer's always actions
+					context.recognizers.push(recognizer);
 					personality.push(context);
 					break;
 
 				case 'recognizer':
-					recognizer = {pattern:found.tokens[2].value,priority:0,options:[]};
+					let matchers = this.formatRecognizerPattern(found.param.pattern);  // translate to format for matching
+					recognizer = { pattern:found.param.pattern, matchers:matchers, options:[], actions:[] };
+					option     = recognizer;  // to collect recognizer's always actions 
 					context.recognizers.push(recognizer);
 					break;
 
 				case 'option':
-					let last      = (found.tokens.length > 1 && (found.tokens[0].value === 'last' || found.tokens[1].value === 'exclusive')) ? true : false;
-					let exclusive = (found.tokens.length > 1 && (found.tokens[0].value === 'exclusive' || found.tokens[1].value === 'exclusive')) ? true : false;
-					option        = {condition:null,last:last,exclusive:exclusive,actions:[]};
+					let fallback     = (found.tokens.length > 1 && (found.tokens[0].value === 'fallback' || found.tokens[1].value === 'nonexclusive')) ? true : false;
+					let nonexclusive = (found.tokens.length > 1 && (found.tokens[0].value === 'nonexclusive' || found.tokens[1].value === 'nonexclusive')) ? true : false;
+					option           = {condition:'',fallback:fallback,nonexclusive:nonexclusive,actions:[]};
 					recognizer.options.push(option);
 					break;
-
-				//case 'say':
-				//	let action = { command:'say', message:found.tokens[2].value, lineNo:found.tokens[0].lineNo };  // TODO: timings
-				//	option.actions.push(action);
-				//	break;
 
 				// Action Commands 
 				default:
@@ -212,31 +195,173 @@ class Moringa {
 		} // end of tokens loop
 		if( err !== '' ) console.log( err );
 		
+		// For each context, sort from longest to shortest..
+		for( var c = 0; c < personality.length; c += 1 ) {
+			var context = personality[c];
+			context.recognizers.sort(( a, b ) => { return b.matchers.length - a.matchers.length });
+		}
 	} // end of importSupplement()
+
+	// Translate string recognizer pattern to matchable version
+	formatRecognizerPattern( string ) {
+		// Tokenize recognizer pattern
+		let syntax = {
+			splitters:[' ','\t','\n','~','`','!','@','#','$','%','^','&','*','(',')','-','+','_','=','{','}','[',']',':',';','"','\'','<','>',',','.','?','/','|','\\'],
+			removes:[' ','\t','\n'],
+			enclosures:[{opener:'[',escaper:'\\',closer:']'}], // [abc] = variable "abc"; [abc:barf] variable "abc" if any value in the "barf" group
+			enclosures:[{opener:'`',escaper:'\\',closer:'`'}]  // litteral text, precisely character by charactet
+		}
+		let matchers = tokenizer( string, syntax, false );
+		
+		// Make variables a single token
+		for( let i = 0; i < matchers.length; i += 1 ) {
+			if( matchers[i] === '[' && matchers[i+2] === ']' ) matchers.splice(i,3,'['+matchers[i+1]+']');
+		}
+		return matchers;
+	}
 
 	exportScript( name = this.name, includeModels = true ) {
 		// TODO
 	}
 
+	/*
+      For each active context:
+        - Perform Always Actions (each begins with nonexclusvie always actions)
+        - Collect Valid Options
+        - On first matching recognizer:
+          - Perform Always Actions
+          - Collect Valid Options
+     */
 	input( message, name = this.name, callback = this.callback ) {
-		console.log('PERSONALITIES:\n' + JSON.stringify(this.personality,null,'  '));  // XXX
-		//let options = this.getOptions( message, this.personality[name] );
-		//let option  = this.getPreferred(options, this.personality[name] );
-		//this.execute( option, this.personality[name] );
+		let personality = this.personality[name];
+		let awareness   = this.interpret( message, personality );
+		console.log( JSON.stringify( awareness, null, '  ' ) );
+		let preferred   = this.preferredOption( awareness, personality );
+		this.performActions( preferred.actions, awareness.variables );
+		callback('todo response');
 	}
 
-	getOptions( message, personality ) {
-		//var options = [];
-		//for( var c = 0; c < personality.length; c += 1 ) {
+	// Returns Awareness from Recogition and Deducations
+	interpret( message, personality ) {
+		// Variable to collecting options and related data in; search stops when recognizer is matched
+		var awareness = {
+			recognizer:false,  // recognizer matched (else undefined)
+			variable:{},       // variables collected
+			options:[],        // valid options collected
+		};
+
+		// For each context..
+		for( var c = 0; c < personality.length; c += 1 ) {
+			// For each recognizer..
+			for( var r = 0; r < personality[c].recognizers.length; r += 1 ) {
+				var recognizer = personality[c].recognizers[r];
+
+				// If the context's always recognizer.. 
+				if( recognizer.pattern === '' ) {
+					// Perform always actions
+					this.performActions( recognizer.actions, personality );
+					// Collect valid options
+					for( var i = 0; i < recognizer.options.length; i += 1 ) {
+						if( this.isOptionValid( option, personality ) ) awareness.options.push(recognizer.options[i]);
+					}
+					continue;
+				}
+
+				// If recognizer is matched, collect all its options then stop collecting.
+				let variable = this.matchRecognizer( message, recognizer, personality );
+				if( variable !== false ) {
+					awareness.recognizer = recognizer.pattern;
+					this.performActions( recognizer.actions, personality );  // perform recognized's always actions
+					for( var name in variable )                             awareness.variable[name] = variable[name];
+					for( var i = 0; i < recognizer.options.length; i += 1 ) awareness.options.push(recognizer.options[i]);
+					break;
+				}
+			} // end of recognizers (r) for loop
+			if( awareness.recognizer !== false ) break;
 			
-		//}
+		} // end of context (c) for loop
+
+		return awareness;
 	}
 
-	getPreferred( options, personality ) {
+	// Returns false else object of collected variables
+	matchRecognizer( message, recognizer, personality ) {
+		var matchers = recognizer.matchers;
+
+		// Tokenize input message
+		let syntax = {	
+			splitters:[' ','\t','\n','~','`','!','@','#','$','%','^','&','*','(',')','-','+','_','=','{','}','[',']',':',';','"','\'','<','>',',','.','?','/','|','\\'],
+			removes:['\t','\n']
+		}
+		let actuals = tokenizer( message, syntax, false );
+
+		// Check how many matchers in actuals (message tokens), in order.. 
+		var m        = 0;
+		var a        = 0;
+		var variable = {};
+		var betweens = '';
+		for( a = 0; a < actuals.length; a += 1 ) {
+			if( !( m < matchers.length ) ) break;
+			var matcher = matchers[m];
+			// If variable..
+			if( matcher[0] === '[' && matcher[matcher.length-1] === ']' ) {
+				// Capture variable until next matcher or end of actuals (of no more matchers)
+				var name  = matchers[m].substr(1,matchers[m].length-2);
+				var value = '';
+				m += 1;
+				while( a < actuals.length ) {
+					if( matchers[m] !== undefined && actuals[a].toLowerCase() === matchers[m].toLowerCase() ) break;
+					value += actuals[a];
+					a += 1;
+				}
+				a -= 1;
+				// TODO: validate if variable has group specified
+				variable[name] = value.trim();
+			} 
+			// Else litteral
+			else {
+				var synonyms = [matcher];
+				// TODO: add any synonyms to synonyms
+				var matches = false;
+				for( var s = 0; s < synonyms.length; s +=1 ) {
+					if( synonyms[s].toLowerCase() === actuals[a].toLowerCase() ) { matches = true; break; }
+				}
+				if( matches ) {
+					// Record any relevant implicit variables between matchers
+					if( m < matchers.length ) variable['before-' + m] = betweens.trim();
+					if( m > 0 ) variable['after-' + (m-1)]            = variable['before-' + m];
+					betweens                                          = ''; 
+
+					// Advance to seek next matcher
+					m += 1;
+				}
+				else {
+					// Did not match, so store as an implicit "between the matchers" variable..
+					betweens += actuals[a];
+				}
+			}
+		} // end of actuals for loop
+		for( a = a; a < actuals.length; a += 1 ) betweens += actuals[a];
+		variable['after-' + (m-1)] = betweens.trim();
+
+		// This Recognizer Matched, if all matchers were found in input message actuals 
+		if( m === matchers.length ) {
+			return variable;
+		} 
+		else { return false; }
 	}
 
-	execute( option, personality ) {
+	isOptionValid( option, personality ) {
+		return true;
 	}
-}
+
+	preferredOption( awareness, personality ) {
+		return {actions:[]};
+	}
+
+	performActions( actions, variables, personality ) {
+	}
+
+} // End of class
 
 exports.Moringa = Moringa;
